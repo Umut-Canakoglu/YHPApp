377d3d9c984251e9f410ec44a902a478113b9594
Umut-Canakoglu <148465244+Umut-Canakoglu@users.noreply.github.com>
2026-04-21 00:01:54 +0300

    Add comments to clarify asteroid movement code

--- Assets/Scripts/AsteroidMovement.cs
+++ Assets/Scripts/AsteroidMovement.cs
@@ -8,14 +8,14 @@ public class AsteroidMovement : MonoBehaviour
     public float massAsteroid;
     public float radiusAsteroid;
     public float orbitAsteroid;
-    public TrailRenderer trailRenderer;
+    public TrailRenderer trailRenderer;//A trail for the asteroid
     public ParticleSystem[] explosionSystems;
     // Start is called before the first frame update
     void Start()
     {
         trailRenderer = GetComponent<TrailRenderer>();
-        asteroidObject = new Asteroid(gameObject, massAsteroid, radiusAsteroid * 0.1f, orbitAsteroid);
-        //Creating an instance of the planet class
+        asteroidObject = new Asteroid(gameObject, massAsteroid, radiusAsteroid * 0.1f, orbitAsteroid); //The trail is affected by the asteroid's size
+        //Creating an instance of the asteroid class
         trailRenderer.widthMultiplier = radiusAsteroid * 0.15f;
     }
 
@@ -35,7 +35,7 @@ public class AsteroidMovement : MonoBehaviour
                 //Using a for loop to iterate through each particle system
                 ParticleSystem instantExp = Instantiate(particle, transform.position, Quaternion.identity);
                 ParticleSystem.ShapeModule pshape = instantExp.shape;
-                pshape.radius = radiusAsteroid * 0.5f;
+                pshape.radius = radiusAsteroid * 0.5f;//The size of the collisions are affected by the object's size
                 instantExp.Play();
                 Destroy(instantExp, 1f);
                 //Destroying the particle effects after a certain time (1s)
